9f8f4b6c0b4f28fc2f09eb8d7dd25d0dcd2fab8a
Andrews Reis <andrewsdosreis@gmail.com>
2019-07-13 20:05:42 -0300

    Add estrutura inicial

---
+++ src/test/java/com/sicredi/pollservice/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.pollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
