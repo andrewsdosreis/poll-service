7a6f5c97e890dabd00e34473383ca146e9b72e19
Andrews Reis <andrewsdosreis@gmail.com>
2019-07-13 21:44:34 -0300

    Add notação @Repository nas interfaces de repositório

--- src/main/java/com/sicredi/pollservice/repository/UserRepository.java
+++ src/main/java/com/sicredi/pollservice/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.sicredi.pollservice.repository;
 import com.sicredi.pollservice.entity.User;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     
 }
--- src/main/java/com/sicredi/pollservice/repository/VoteRepository.java
+++ src/main/java/com/sicredi/pollservice/repository/VoteRepository.java
@@ -3,7 +3,9 @@ package com.sicredi.pollservice.repository;
 import com.sicredi.pollservice.entity.Vote;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VoteRepository extends CrudRepository<Vote, Integer> {
 
 }
