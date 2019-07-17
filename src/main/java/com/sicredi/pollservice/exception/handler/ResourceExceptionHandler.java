d899bf22a388f1e2d2f6fa1c69ab62c19f68c4e9
Andrews Reis <andrewsdosreis@gmail.com>
2019-07-16 23:44:05 -0300

    Add exceptions no tratamento de erros

--- src/main/java/com/sicredi/pollservice/exception/handler/ResourceExceptionHandler.java
+++ src/main/java/com/sicredi/pollservice/exception/handler/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import com.sicredi.pollservice.context.logger.Logger;
 import com.sicredi.pollservice.exception.BusinessException;
 import com.sicredi.pollservice.exception.ResourceNotFoundException;
 
+import org.springframework.amqp.AmqpException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,7 +23,7 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ErrorHandler> objectNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		ErrorHandler err = new ErrorHandler(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
-				"Not found", e.getMessage(), request.getRequestURI());
+				"NotFound Exception", e.getMessage(), request.getRequestURI());
 		logger.error(err.toString());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
@@ -29,8 +31,32 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<ErrorHandler> businessException(BusinessException e, HttpServletRequest request) {
 		ErrorHandler err = new ErrorHandler(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
-				"Business exception", e.getMessage(), request.getRequestURI());
+				"Business Exception", e.getMessage(), request.getRequestURI());
 		logger.error(err.toString());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(NumberFormatException.class)
+	public ResponseEntity<ErrorHandler> numberFormatException(NumberFormatException e, HttpServletRequest request) {
+		ErrorHandler err = new ErrorHandler(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"NumberFormat Exception", e.getMessage(), request.getRequestURI());
+		logger.error(err.toString());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmqpException.class)
+	public ResponseEntity<ErrorHandler> amqpException(AmqpException e, HttpServletRequest request) {
+		ErrorHandler err = new ErrorHandler(System.currentTimeMillis(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				"Amqp Exception", e.getMessage(), request.getRequestURI());
+		logger.error(err.toString());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ErrorHandler> dataIntegrity(DataIntegrityViolationException e, HttpServletRequest request) {
+		ErrorHandler err = new ErrorHandler(System.currentTimeMillis(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				"DataIntegrityViolation Exception", e.getMessage(), request.getRequestURI());
+		logger.error(err.toString());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
 	}
 }
